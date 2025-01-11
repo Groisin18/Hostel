249d8c098c6bead4447b6813e33845b6ef6f8ca9
Groisin18 <iliagrois@gmail.com>
2025-01-11 10:57:26 +0800

    Minor changes

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import os
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters.command import Command
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
@@ -63,6 +63,21 @@ def save_user_data(data):
     with open(USER_DATA_FILE, "w", encoding='UTF-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
+
+# @dp.message_handler(commands=['start'])
+# async def send_welcome(message: types.Message):
+#    kb = [
+#        [
+#            KeyboardButton(text="Сможешь повторить это?"),
+#            KeyboardButton(text="А это?")
+#        ],
+#    ]
+#    keyboard = ReplyKeyboardMarkup(keyboard=kb)
+
+#    await message.reply("Привет!\nЯ Эхобот от Skillbox!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.", reply_markup=keyboard)
+
+
+
 @dp.message(Command("start"))
 async def send_welcome(message: Message):
     user_data = load_user_data()
@@ -70,13 +85,15 @@ async def send_welcome(message: Message):
 
     if user_id in user_data:
         user_info = user_data[user_id]
-        await message.answer(
-            f"Привет, {user_info['name']}! Ваша заявка уже зарегистрирована."
-        )
+        await message.answer(f"Привет, {user_info['name']}! Ваша заявка уже зарегистрирована.")
     else:
-        await message.answer(
-            "Привет! Пожалуйста, отправьте свои ФИО и возраст в формате:\nФИО, возраст"
-        )
+        kb = [
+        [
+           KeyboardButton(text="Зарегистрироваться"),
+        ],
+        ]
+        keyboard = ReplyKeyboardMarkup(keyboard=kb)
+        await message.answer("Привет! Пожалуйста, отправьте свои ФИО и возраст в формате:\nФИО, возраст", reply_markup=keyboard)
 
 @dp.message(F.text)
 async def handle_user_data(message: Message):
